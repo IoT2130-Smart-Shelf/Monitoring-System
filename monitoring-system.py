a8384c6670ee2c8663b369750c98ebfc47736186
Juan Sebastián Barreto Jiménez <juan_barreto@javeriana.edu.co>
2021-09-24 19:08:10 -0500

    Medicion de distacinas

---
+++ monitoring-system.py
@@ -0,0 +1,98 @@
+"""Monitoring System Smart Shelf
+
+Members:
+    - Juan Sebastián Barreto Jimenéz
+    - Carolina María Burgos Anillo
+    - Edwin Alejandro Caicedo Palacios
+"""
+
+# Imports of the necessary libraries
+from mqtt_thingspeak import send_mqtt_thingsSpeak
+from Laser_Sensor.laser_linear import laser_linearization
+import adafruit_vl53l0x as av
+import busio
+import RPi.GPIO as GPIO
+import time
+
+# Initialize number pins of I2C for Laser Sensor
+SCL = 3
+SDA = 2
+
+# Initialize I2C bus and sensor.
+i2c = busio.I2C(SCL, SDA)
+laser_sensor = av.VL53L0X(i2c)
+
+# Initialize numeric pins for ultrasound sensors
+TRIG_ONE = 23
+ECHO_ONE = 24
+TRIG_TWO = 17
+ECHO_TWO = 27
+TRIG_THREE = 5
+ECHO_THREE = 6
+
+# Initialize GPIO for ultrasound sensors
+# Ultrasound sensor one
+GPIO.setup(TRIG_ONE, GPIO.OUT)
+GPIO.setup(ECHO_ONE, GPIO.IN)
+# Ultrasound sensor two
+GPIO.setup(TRIG_TWO, GPIO.OUT)
+GPIO.setup(ECHO_TWO, GPIO.IN)
+# Ultrasound sensor three
+GPIO.setup(TRIG_THREE, GPIO.OUT)
+GPIO.setup(ECHO_THREE, GPIO.IN)
+
+while True:
+    try:
+        # Read laser sensor
+        distance_laser = laser_linearization(laser_sensor.range)
+
+        if(distance_laser < 810):
+            print("Laser Sensor Range: {0}cm".format(distance_laser))
+            # Read ultrasound sensors
+            GPIO.output(TRIG_ONE, GPIO.LOW)
+            GPIO.output(TRIG_TWO, GPIO.LOW)
+            GPIO.output(TRIG_THREE, GPIO.LOW)
+
+            print("Waiting for ultrasound sensors to settle") 
+
+            time.sleep(2)
+            print("Calculating distance of ultrasound sensors")
+            GPIO.output(TRIG_ONE, GPIO.HIGH)
+            GPIO.output(TRIG_TWO, GPIO.HIGH)
+            GPIO.output(TRIG_THREE, GPIO.HIGH)
+            time.sleep(0.00001)
+            GPIO.output(TRIG_ONE, GPIO.LOW)
+            GPIO.output(TRIG_TWO, GPIO.LOW)
+            GPIO.output(TRIG_THREE, GPIO.LOW)
+            
+            while GPIO.input(ECHO_ONE)==0:
+                pulse_start_one = time.time()
+            while GPIO.input(ECHO_ONE)==1:
+                pulse_end_one = time.time()
+            while GPIO.input(ECHO_TWO)==0:
+                pulse_start_two = time.time()
+            while GPIO.input(ECHO_TWO)==1:
+                pulse_end_two = time.time()
+            while GPIO.input(ECHO_THREE)==0:
+                pulse_start_three = time.time()
+            while GPIO.input(ECHO_THREE)==1:
+                pulse_end_three = time.time()
+            
+            pulse_duration_one = pulse_end_one - pulse_start_one
+            distance_ultrasound_one = round(pulse_duration_one*17150,2)
+            pulse_duration_two = pulse_end_two - pulse_start_two
+            distance_ultrasound_two = round(pulse_duration_two*17150,2)
+            pulse_duration_three = pulse_end_three - pulse_start_three
+            distance_ultrasound_three = round(pulse_duration_three*17150,2)
+
+            print("Distance Ultrasound One: ", distance_ultrasound_one," cm")
+            print("Distance Ultrasound Two: ", distance_ultrasound_two," cm")
+            print("Distance Ultrasound Three: ", distance_ultrasound_three," cm")
+
+            # Send data to thingSpeak by mqtt
+            send_mqtt_thingsSpeak(distance_ultrasound_one, distance_ultrasound_two, distance_ultrasound_three, distance_laser)
+
+    except (KeyboardInterrupt):
+        break
+    finally:
+        GPIO.cleanup()
--- mqtt_thingspeak.py
+++ mqtt_thingspeak.py
@@ -1,47 +1,28 @@
-from __future__ import print_function
+# Imports of the necessary libraries
 import paho.mqtt.publish as publish
+import ssl
 
-channelID = "1481979" # Channel ID of ThingSpeak
-
-writeApiKey = "R6KP5RUYDZOS6XKO" # Write API Key of ThingSpeak
-
-useUnsecuredTCP = False
-
-useUnsecuredWebsockets = False
-
-useSSLWebsockets = True
-
-mqttHost = "mqtt.thingspeak.com"
-
-if useUnsecuredTCP:
-    tTransport = "tcp"
-    tPort = 1883
-    tTLS = None
-
-if useUnsecuredWebsockets:
-    tTransport = "websockets"
-    tPort = 80
-    tTLS = None
-
-if useSSLWebsockets:
-    import ssl
+#   Function: send_mqtt_thingsSpeak() 
+#   Purpose: Send data from two fields by mqtt to thingspeak
+#   Argument:
+#       distanceSound: Measured distance by ultrasound sensor
+#       distanceLaser: Measured distance by laser sensor
+#   Return:
+#       Confirmation or error message
+def send_mqtt_thingsSpeak(distanceSoundOne, distanceSoundTwo, distanceSoundThree, distanceLaser):
     tTransport = "websockets"
     tTLS = {'ca_certs':"/etc/ssl/certs/ca-certificates.crt",'tls_version':ssl.PROTOCOL_TLSv1}
     tPort = 443
+    channelID = "1481979" # Channel ID of ThingSpeak
+    writeApiKey = "R6KP5RUYDZOS6XKO" # Write API Key of ThingSpeak
+    mqttHost = "mqtt.thingspeak.com"
+    topic = "channels/" + channelID + "/publish/" + writeApiKey
 
-topic = "channels/" + channelID + "/publish/" + writeApiKey
-
-distanceSound = 300
-distanceLaser = 100
-print (" Distance Sound =", distanceSound, "   Distance Laser =", distanceLaser)
-
-# build the payload string
-tPayload = "field1=" + str(distanceSound) + "&field2=" + str(distanceLaser)
-# attempt to publish this data to the topic
-try:
-    publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
-except (KeyboardInterrupt):
-    #break
-    pass
-except (Exception):
-    print ("There was an error while publishing the data.")
+    # build the payload string
+    tPayload = "field1=" + str(distanceSoundOne) + "&field2=" + str(distanceSoundTwo)  + "&field3=" + str(distanceSoundThree)  + "&field4=" + str(distanceLaser)
+    # attempt to publish this data to the topic
+    try:
+        publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
+        return "Datos enviados"
+    except (Exception):
+        return "Hubo un error al publicar los datos."
