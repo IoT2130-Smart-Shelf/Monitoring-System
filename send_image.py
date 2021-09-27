98002985b7f3636223c11e54bfad26cb4470facf
Edwin99pal <81887987+Edwin99pal@users.noreply.github.com>
2021-09-26 21:22:52 -0500

    Update send_image.py
    Se realiza el comentado del codigo

--- send_image.py
+++ send_image.py
@@ -1,24 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 26 19:40:26 2021
+    Script para realizar el envio iterado de fotografias entre dos tarjetas raspberry cada cierto tiempo
+    haciendo uso del protocolo SSHv2 para el envio de archivos de forma segura.
+
+@author: edwin
+"""
+
 from datetime import date
 from datetime import datetime
 from picamera import PiCamera
-import threading
-from picamera import PiCamera
-import os
+import threading                # modulo para implementacion de temporizadores
+from picamera import PiCamera   # modulo para el manejo de la camara raspberry
+import os                       # modulo para el borrado de archivos
+import paramiko    # libreria para poder utilizar el protocolo SSHv2 entre las raspbrerys
 
-import paramiko
-"""
-    Para la instalación de paramiko usar pip3 install paramiko
-    Upload file, can not upload directory.
-    :param host: sftp server host name or ip.
-    :param port: sftp server listening port nubmer.
-    :param user: sftp user name
-    :param password: sftp account password
-    :param server_path: remote server file path，for example：/root/test/test.txt
-    :param local_path: local file path (c:/test.txt)
-    :param timeout: upload connection timeout number ( an integer value, default is 10 )
-    :return: bool
-"""
+
 def sftp_upload_file(host, port, user, password, server_path, local_path, timeout=10):
+    '''
+    Parameters
+    ----------
+    host : TYPE
+         sftp server host name or ip.
+    port : TYPE
+        sftp server listening port nubmer.
+    user : TYPE
+        sftp user name.
+    password : TYPE
+        sftp account password.
+    server_path : TYPE
+        remote server file path，for example：/root/test/test.txt
+    local_path : TYPE
+        local file path (c:/test.txt).
+    timeout : TYPE, optional
+        upload connection timeout number ( an integer value, default is 10 ).
+        
+    Returns
+    -------
+    bool
+        True for correct send, False for incorret send.
+
+    '''
     try:
         # create transport object.
         t = paramiko.Transport((host, port))
@@ -40,37 +62,62 @@ def sftp_upload_file(host, port, user, password, server_path, local_path, timeou
     except Exception as e:
         print(e)
         return False
-    
-#sftp_upload_file('192.168.20.61',22,'pi','carolina','/home/pi/Desktop/image/ImagesCamera.jpg','/home/pi/Desktop/image.jpg')
-
 
 camera = PiCamera()
-camera.rotation = 180
+camera.rotation = 180   # rotacion de la imagen tomada
 
 def currentTime():
+    '''
+    Genera la estampa de tiempo de la hora de la toma de la imagen
+
+    Returns
+    -------
+    current_time : String
+        Hora_Minuto_Segundo.
+
+    '''
     now = datetime.now()
     current_time = now.strftime("%H_%M_%S")
     print("Current Time =", current_time)
     return current_time
 
 def currentDate():
+    '''
+    Genera la estampa de tiempo del dia de la toma de la imagen
+
+    Returns
+    -------
+    d1 : String
+        Day_Month_year.
+
+    '''
     today = date.today()
     d1 = today.strftime("%d_%m_%Y")
     print("d1 =", d1)
     return d1
 
 def printit():
-  threading.Timer(5, printit).start()
-  time = currentTime()
-  date = currentDate()
-  camera.capture('/home/pi/Desktop/ImagenesEstante/EstanteDD'+str(date)+'HH'+str(time)+'.jpg')
-  sftp_upload_file('192.168.20.61',22,'pi','carolina',
-                   '/home/pi/Desktop/ImagesCamera/EstanteDD'+str(date)+'HH'+str(time)+'.jpg',
-                   '/home/pi/Desktop/ImagenesEstante/EstanteDD'+str(date)+'HH'+str(time)+'.jpg')
-  os.remove('/home/pi/Desktop/ImagenesEstante/EstanteDD'+str(date)+'HH'+str(time)+'.jpg')
-  print("Imagen enviada al nodo")
+    '''
+    Inicializa un timer y envia la imagen de una raspberry a otra por SSH cuando el timer se cumple
+
+    Returns
+    -------
+    None.
+
+    '''
+    threading.Timer(5, printit).start()    # se inicializa el timer
+    time = currentTime()                   # Genero el texto de la hora
+    date = currentDate()                   # Genero el texto de la fecha
+    # Se realiza la captura de la imagen
+    camera.capture('/home/pi/Desktop/ImagenesEstante/EstanteDD'+str(date)+'HH'+str(time)+'.jpg')
+    # Se realiza la transferencia del archivo por SSH
+    sftp_upload_file('192.168.20.61',22,'pi','carolina',
+                       '/home/pi/Desktop/ImagesCamera/EstanteDD'+str(date)+'HH'+str(time)+'.jpg',
+                       '/home/pi/Desktop/ImagenesEstante/EstanteDD'+str(date)+'HH'+str(time)+'.jpg')
+    # Se elimina el archivo luego del envio
+    os.remove('/home/pi/Desktop/ImagenesEstante/EstanteDD'+str(date)+'HH'+str(time)+'.jpg')
+    print("Imagen enviada al nodo")
 
 camera.capture('/home/pi/Desktop/ImagenesEstante/estante.jpg')
 os.remove('/home/pi/Desktop/ImagenesEstante/estante.jpg')
-printit()
-
+printit()   # se ejecuta iteradamente el temporizador
